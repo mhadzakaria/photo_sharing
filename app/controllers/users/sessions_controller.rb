595c46702ac1068f271ba1cb5fcee4ad93fb3640
3lviend <henryss_10@yahoo.com>
2020-06-25 10:21:30 +0700

    add devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::SessionsController < Devise::SessionsController
+  def respond_to_on_destroy
+    respond_to do |format|
+      format.all  { head :no_content }
+      format.html { redirect_to new_user_session_url }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+    end
+  end
+end
